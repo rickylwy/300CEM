a7ff79f1ca68a9603fd3c848d02eae3ddfd1c2a6
rickylwy <leungwy511@gmail.com>
2020-10-23 06:31:37 +0800

    Implemented real time location update logic

--- app/src/main/java/com/example/health_booster/MainActivity.java
+++ app/src/main/java/com/example/health_booster/MainActivity.java
@@ -1,18 +1,42 @@
 package com.example.health_booster;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private MapView mapView;
+    private GoogleMap googleMap;
+    private Location location;
+    private Circle circle;
+
+    private final Handler locationHandler = new Handler();
+    private final Runnable locationUpdater = new Runnable() {
+        @Override
+        public void run() {
+            updateLocation();
+            locationHandler.postDelayed(this, 10000);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +54,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
+    public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
 
         Bundle mapViewBundle = outState.getBundle("MapViewBundleKey");
@@ -41,6 +65,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         mapView.onSaveInstanceState(mapViewBundle);
     }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -58,25 +83,68 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onStop();
         mapView.onStop();
     }
+
     @Override
     protected void onPause() {
         mapView.onPause();
         super.onPause();
     }
+
     @Override
     protected void onDestroy() {
         mapView.onDestroy();
         super.onDestroy();
     }
+
     @Override
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
     }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.setMinZoomPreference(12);
+        this.googleMap = googleMap;
+        googleMap.setMinZoomPreference(15);
         LatLng ny = new LatLng(22.438050, 114.014290);
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(ny));
+
+        locationHandler.postDelayed(locationUpdater, 0);
+    }
+
+    protected void updateLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            Task<Location> locationResult = LocationServices.getFusedLocationProviderClient(this).getLastLocation();
+            locationResult.addOnCompleteListener(this, new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if (task.isSuccessful()) {
+                        // Set the map's camera position to the current location of the device.
+                        location = (Location) task.getResult();
+                        if (location != null)
+                        {
+                            if (circle != null)
+                            {
+                                circle.remove();
+                            }
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            circle = googleMap.addCircle(new CircleOptions()
+                                    .center(latLng)
+                                    .radius(30)
+                                    .strokeColor(Color.RED)
+                                    .fillColor(Color.BLUE));
+                            googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                        }
+                    }
+                }
+            });
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        }
     }
 }
