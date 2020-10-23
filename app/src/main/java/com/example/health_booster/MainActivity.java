93d859d72ca75d4649da46909470f10dc4065e43
rickylwy <leungwy511@gmail.com>
2020-10-23 08:41:12 +0800

    Added vast comments

--- app/src/main/java/com/example/health_booster/MainActivity.java
+++ app/src/main/java/com/example/health_booster/MainActivity.java
@@ -39,11 +39,13 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private Marker marker;
     private SharedPreferences sharedPreferences;
 
+    //Here use Handler class for continuous location update logic
     private final Handler locationHandler = new Handler();
     private final Runnable locationUpdater = new Runnable() {
         @Override
         public void run() {
             updateLocation();
+            //Do every 1 second
             locationHandler.postDelayed(this, 1000);
         }
     };
@@ -53,10 +55,13 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Get Data persistence
         sharedPreferences = getPreferences(Context.MODE_PRIVATE);
 
+        //Update score display from data persistence
         updateCurrentScore(getCurrentScore());
 
+        //MapView Init codes
         Bundle mapViewBundle = null;
         if (savedInstanceState != null) {
             mapViewBundle = savedInstanceState.getBundle("MapViewBundleKey");
@@ -71,6 +76,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
 
+        //MapView codes
         Bundle mapViewBundle = outState.getBundle("MapViewBundleKey");
         if (mapViewBundle == null) {
             mapViewBundle = new Bundle();
@@ -118,40 +124,55 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        //Init MapView when ready, set GoogleMap to global variable for others methods to access
         this.googleMap = googleMap;
         googleMap.setMinZoomPreference(15);
 
+        //Start of updating location
         locationHandler.postDelayed(locationUpdater, 0);
     }
 
     protected void updateLocation() {
+        //Core App logic
+        //Check if GPS permission is granted. If not granted, request for it
+        //If GPS permission is never granted, the user's location cannot be updated
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
         {
+            //Get device's current location by Google Service API
             Task<Location> locationResult = LocationServices.getFusedLocationProviderClient(this).getLastLocation();
+            //Async get location, need use callback to handle
             locationResult.addOnCompleteListener(this, (OnCompleteListener<Location>) task -> {
                 if (task.isSuccessful()) {
-                    // Set the map's camera position to the current location of the device.
-                    if (circle != null)
-                    {
-                        circle.remove();
-                    }
+                    //Location can be gotten successfully
                     if (task.getResult() != null)
                     {
+                        // Remove last user's location if exist
+                        if (circle != null)
+                        {
+                            circle.remove();
+                        }
                         latLng = new LatLng(task.getResult().getLatitude(), task.getResult().getLongitude());
+                        //Add back user's location circle mark
                         circle = googleMap.addCircle(new CircleOptions()
                                 .center(latLng)
                                 .radius(30)
                                 .strokeColor(Color.RED)
                                 .fillColor(Color.BLUE));
+                        //Center user's location
                         googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                        //If no tasks yet, generate one
                         if (taskLatLng == null)
                         {
                             updateTask(null);
                         }
+                        //Else check if the task is completed
                         else if (Math.abs(taskLatLng.longitude - latLng.longitude) < 0.00050 && Math.abs(taskLatLng.latitude - latLng.latitude) < 0.00050)
                         {
+                            //Update score
                             updateCurrentScore(getCurrentScore() + 1);
+                            //Generate another task
                             updateTask(null);
+                            //Notify user for task completions
                             Toast.makeText(getApplicationContext(), getString(R.string.task_completed), Toast.LENGTH_LONG).show();
                         }
                     }
@@ -160,6 +181,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         else
         {
+            //Check if GPS permission is granted. If not granted, request for it
             ActivityCompat.requestPermissions(this,
                     new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                     1);
@@ -168,10 +190,13 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     public void updateTask(View view)
     {
+        //Generating tasks requires user's current location so check for it first
         if (latLng != null)
         {
+            //Generate random location, there will be a bonus for users seldom when the task is too close to current location
             taskLatLng = new LatLng(Math.random()*0.00500*(1 - (int)(Math.random()*3)) + latLng.latitude,
                     Math.random()*0.00500*(1 - (int)(Math.random()*3)) + latLng.longitude);
+            //Force replace current task
             if (marker != null)
             {
                 marker.remove();
@@ -182,14 +207,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     protected int getCurrentScore()
     {
+        // Get score from data persistence
         return sharedPreferences.getInt("totalScore", 0);
     }
 
     protected void updateCurrentScore(int newScore)
     {
+        //Update new score to data persistence
         sharedPreferences.edit().putInt("totalScore", newScore).commit();
+        //Also update the UI
         ((TextView)findViewById(R.id.textView)).setText(
                 ((TextView)findViewById(R.id.textView)).getText().toString()
-                        .replaceAll(":.*", ": " + newScore));
+                        .replaceAll(":.*+", ": " + newScore));
     }
 }
